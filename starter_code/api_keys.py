909ca77f913a914e98598b4172aad83052a25e02
Cheyenne M <76500260+martiche93@users.noreply.github.com>
2021-06-01 21:41:23 -0700

    update

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "62574cbc6a0e505aece73bc903ee49d8"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCJCGbmJMfwhnZOUWpvUPMRjOMj80T2LXk"
